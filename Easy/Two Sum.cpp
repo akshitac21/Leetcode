381a5d0ec1adef745e93262b9b3ddaebcc5b81c1
Akshita Choudhury <aksxclint@gmail.com>
2022-12-06 18:52:33 +0530

    Create

---
+++ Easy/Two Sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        const int size = nums.size();
+
+        vector<int> sol;
+        
+        for(int i=0; i<size; i++)
+        {
+            for(int j=i+1; j<size; j++)
+            {
+                if(nums[i]+nums[j] == target)
+                {
+                    sol.push_back(i);
+                    sol.push_back(j);
+                }
+            }
+        }
+        return sol;
+    }    
+};
